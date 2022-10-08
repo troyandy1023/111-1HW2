872ee7ebd327e15b164c8296494a377dcd442b26
troyandy1023 <alicebobo223@gmail.com>
2022-10-08 19:48:02 +0800

    (手可以借給我嗎) YA I &$"&@ the same 都好像沒有變 你來 changed 還是討厭下雨天 還是不愛認錯 脾氣是硬了點 這我都清楚 但我沒有辦法改變 我後悔高中裝£¥{*&/#%的排氣管 想努力賺錢養妳 卻養成了壞習慣 我還想帶到妳 到處含 到處帶你玩 without you it feels like 情人節沒有愛人 耶 像台相機沒有快門 耶 像上出所沒有檯燈 耶 without you it feels like 呃 \%¥ we刀no嘿嘿 It be like youtube without no 呸踢斯 Or 呃 Chen without no pages without you it fish (合唱嗎) 凹you I 東往那 be alone欸 想念妳的每個角度 如果我們還能夠重來 Oh You dont know how I feel inside 手 I don't wanna live 凹 you 我沒有辦法! 再拼 too that 重蹈覆轍 I can too that 沒得負責 oh damn too bad I的揪rua 味U味 先把我的面子放兩邊 榨乾我就像搶劫 Goddamn I got 內不內 然後進去 拖馬燙 呦 救救窩 I call my friends 訴苦 說了好幾遍 他們叫我振作點 I know 勉 fuckin care 幫我刪了instagram pose i don't wanna 席 讓你神通 with me 我乾掉哼 李行 just wanna noise 我們之間太多的我捨不得放手耶 失去妳就像失去我的麥克風 我不要我的fans 不要IG的followers呃 顆 (尖叫聲) I don't wanna live we凹you I don't wanna be alone and 想念妳的每個角度 如果我們還能夠重來 Oh You don't know how I feel inside 手 (借給我) 凹 you 我沒有辦法 I don't wanna 李we凹 you I don't wanna be alone 欸 想念妳的每個角度 如果我們還能夠重來 Oh You don't know how I feel inside So I don't wanna 李we凹 you 我沒有辦法 (謝謝)

---
+++ 111-1HW2/Bomb.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1HW2
+{
+    public partial class Bomb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int[] ia_Mindex = new int[10] { 0, 7, 13, 28, 44, 62, 74, 75, 87, 90 };
+            int[] o = new int[50] {1,2,3,4,6,8,10,11,12,14,16,19
+            ,22,23,24,27,29,33,34,35,37,38,39,43,45,51,52,54,55
+            ,61,66,71,72,78,77,80,81,83,88,91,96,97,98,100,101,102,103,104,105,106};
+            int[] s = new int[11] {17,18,53,63,64,65,73,76,84,85,86};
+            char[,] ia_Map = new char[11, 11];
+
+
+            /*for (int a = 0; a < 10; a++)
+            {
+                int b = n[a] / 10;
+                int s = n[a] % 10;
+                ia_Map[b, s] = '1';
+            }*/
+
+            for (int i_Row = 0; i_Row < 11; i_Row++)
+            {
+                for (int a = 0; a < 50; a++)
+                {
+                    int b = o[a] / 10;
+                    int p = o[a] % 10;
+                    ia_Map[b, p] = '1';
+                }
+                for (int y = 0; y < 11; y++)
+                {
+                    int q = s[y] / 10;
+                    int w = s[y] % 10;
+                    ia_Map[q, w] = '2';
+                }
+                for (int i_Col = 0; i_Col < 10; i_Col++)
+                {
+
+                    ia_Map[i_Row, i_Col] = '0';
+                }
+            }
+            for (int i_Ct = 0; i_Ct < 10; i_Ct++)
+            {
+                int i_Row = ia_Mindex[i_Ct] / 10;
+                int i_Col = ia_Mindex[i_Ct] % 10;
+                ia_Map[i_Row, i_Col] = '*';
+            }
+
+            for (int i_Row = 0; i_Row < 10; i_Row++)
+            {
+                for (int i_Col = 0; i_Col < 10; i_Col++)
+                {
+                    if (ia_Map[i_Row, i_Col] == ' ')
+                    {
+                        Response.Write("&nbsp");
+                    }
+                    else
+                    {
+                        Response.Write(ia_Map[i_Row, i_Col]+" "+"|");
+                    }
+                }
+                Response.Write("<br />");
+
+            }
+        }
+    }
+}
